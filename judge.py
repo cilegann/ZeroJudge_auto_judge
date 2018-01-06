64db9b209e40637458ba42f868562b5b004c7500
Sean Chiu <s571991@gmail.com>
2018-01-07 01:35:57 +0800

    bug fixed

--- judge.py
+++ judge.py
@@ -13,42 +13,39 @@ with open(todoF,'r') as file:
     todo=file.readlines()[0].split(',')
 with open(loginF,'r') as file:
     login=file.readlines()[0].split(',')
+
 scores=[]
 
 session_requests = requests.session()
 head = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
+
 login_url='https://zerojudge.tw/Login'
-payload = {
+login_payload = {
 	"account": login[0], 
 	"passwd": login[1], 
 }
-
 result = session_requests.post(
 	login_url, 
-	data = payload, 
+	data = login_payload, 
 	headers = head
 )
 
 for acc in range(len(accounts)):
     scores.append([])
     for tod in range(len(todo)):
-        payloads ={
-            'problemid':todo[tod],
-            'account':accounts[acc]
-        }
         res = session_requests.get('https://zerojudge.tw/Submissions?problemid='+todo[tod]+'&account='+accounts[acc],headers=head, verify=False)
         c=res.text
         if (c.find('<span class="acstyle">AC</span>')>0):
-            print(c.find('<span class="acstyle">AC</span>'))
             scores[acc].append('v')
         else:
             scores[acc].append('')
         time.sleep(0.1)
-session_requests.close()
-result = session_requests.post(
+
+session_requests.post(
 	'https://zerojudge.tw/Logout', 
 	headers = head
 )
+session_requests.close()
 with open(resultF,'w') as file:
     for i in range(len(todo)):
         file.write(',')
