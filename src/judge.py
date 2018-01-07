22dca256d81756aa016ff2440f67877888725d50
cilegann <s571991@gmail.com>
2018-01-07 19:59:58 +0800

    change way of reading account and passwd return error when login is not successful

--- src/judge.py
+++ src/judge.py
@@ -2,57 +2,78 @@ import requests
 import sys
 import os
 import time
+import getpass
 accountsF=sys.argv[1]
 todoF=sys.argv[2]
-loginF='./data/login'
 resultF=sys.argv[3]
-with open(accountsF,'r') as file:
-    accounts=file.readlines()[0].split(',')
-with open(todoF,'r') as file:
-    todo=file.readlines()[0].split(',')
-with open(loginF,'r') as file:
-    login=file.readlines()[0].split(',')
-
-scores=[]
-
-session_requests = requests.session()
 head = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
 
-login_url='https://zerojudge.tw/Login'
-login_payload = {
-	"account": login[0], 
-	"passwd": login[1], 
-}
-result = session_requests.post(
-	login_url, 
-	data = login_payload, 
-	headers = head
-)
+def getAccount():
+    user= input(">>> Valid account to login: ")
+    pwd = getpass.getpass()
+    return([user,pwd])
 
-for acc in range(len(accounts)):
-    scores.append([])
-    for tod in range(len(todo)):
-        res = session_requests.get('https://zerojudge.tw/Submissions?problemid='+todo[tod]+'&account='+accounts[acc],headers=head, verify=False)
-        c=res.text
-        if (c.find('<span class="acstyle">AC</span>')>0):
-            scores[acc].append('v')
-        else:
-            scores[acc].append('')
-        time.sleep(0.1)
+def doLogin(session_request):
+    login=getAccount()
+    login_url='https://zerojudge.tw/Login'
+    login_payload = {
+        "account": login[0], 
+        "passwd": login[1], 
+    }
+    result = session_requests.post(
+        login_url, 
+        data = login_payload, 
+        headers = head
+    )
+    if result.text=='{"currentPage":"./"}':
+        return session_request,True
+    else:
+        return session_request,False
 
-session_requests.post(
-	'https://zerojudge.tw/Logout', 
-	headers = head
-)
-session_requests.close()
-with open(resultF,'w') as file:
-    for i in range(len(todo)):
-        file.write(',')
-        file.write(todo[i])  
-    file.write("\n")
-    for i in range(len(accounts)):
-        file.write(accounts[i])
-        for j in range(len(todo)):
-            file.write(',')
-            file.write(scores[i][j])
-        file.write('\n')
+def getScores(session_request,accounts,todo):
+    scores=[]
+    for acc in range(len(accounts)):
+        print("Getting score of "+accounts[acc])
+        scores.append([])
+        for tod in range(len(todo)):
+            print("  "+todo[tod],end='\r')
+            res = session_requests.get('https://zerojudge.tw/Submissions?problemid='+todo[tod]+'&account='+accounts[acc],headers=head, verify=False)
+            c=res.text
+            if (c.find('<span class="acstyle">AC</span>')>0):
+                scores[acc].append('v')
+            else:
+                scores[acc].append('')
+            time.sleep(0.1)
+        print("  Done.")
+    return scores
+
+if __name__ == "__main__":
+    with open(accountsF,'r') as file:
+        accounts=file.readlines()[0].split(',')
+    with open(todoF,'r') as file:
+        todo=file.readlines()[0].split(',')
+    session_requests = requests.session()
+    session_resuests,status = doLogin(session_requests)
+    if status is False:
+        print("Login Failed.")
+    else:
+        scores = getScores(session_requests,accounts,todo)
+        session_requests.post(
+            'https://zerojudge.tw/Logout', 
+            headers = head
+        )
+        session_requests.close()
+
+        with open(resultF,'w') as file:
+            for i in range(len(todo)):
+                file.write(',')
+                file.write(todo[i])  
+            file.write("\n")
+            for i in range(len(accounts)):
+                file.write(accounts[i])
+                for j in range(len(todo)):
+                    file.write(',')
+                    file.write(scores[i][j])
+                file.write('\n')
+        print("Result saved as "+resultF)
+        print("Jod done at "+time.asctime( time.localtime(time.time()) ))
