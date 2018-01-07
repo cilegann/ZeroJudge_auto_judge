8ff94139c879c008bf92d51132a4dbbe63daf4bf
Sean Chiu <s571991@gmail.com>
2018-01-07 20:08:15 +0800

    Update judge.py

--- src/judge.py
+++ src/judge.py
@@ -3,8 +3,8 @@ import sys
 import os
 import time
 import getpass
-accountsF='./data'+sys.argv[1]
-todoF='./data'+sys.argv[2]
+accountsF='./data/'+sys.argv[1]
+todoF='./data/'+sys.argv[2]
 resultF=sys.argv[3]
 head = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
 
